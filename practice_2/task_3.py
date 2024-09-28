ee5f5216e37de2fb210838c83bb0bd6d60af387e
mironovde <mironov.de@phystech.edu>
2024-09-29 00:00:09 +0300

    test_native_vscode_commit

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,9 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
+import numpy as np
 
-
-#code here
-
+weight = float(input())
+height = float(input())
+BMI = np.round(weight/height**2, 3)
+print(BMI)
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Develop a program that asks the user for an integer 4-digit number and calculates the sum of its constituent digits. For example, if the user enters the number 3141, the program should output the following result:
+Develop a program that asks the user for an integer 4-digit number and calculates the sum of its constituent digits. 
+For example, if the user enters the number 3141, the program should output the following result:
 3 + 1 + 4 + 1 = 9
 
 """
+print('Input 4-digit number pls')
+str = input()
+
+try:
+    digits = [int(i) for i in str]
+    print(f"{' + '.join(str)} = {sum(digits)}")
+except:
+    print('smth is wrong with the input')
+
